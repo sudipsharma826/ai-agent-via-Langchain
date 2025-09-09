27a878a1071dd24db0c1fec02a257e7b004c9dfb
Sudip Sharma <sudpokhara826@gmail.com>
2025-09-09 09:44:10 +0545

    basic created the workflow of the agnet

--- index.ts
+++ index.ts
@@ -1,6 +1,25 @@
+import { StateGraph,MessagesAnnotation} from '@langchain/langgraph';
 import readline from 'node:readline/promises';
 
+interface MessageState {
+    messages: string[];
 
+}
+
+
+//Building a agent model and functionality
+function agentModel(state: MessageState){
+    console.log("calling agent");
+    return state;
+}
+
+//Build the workflow uisng LangGraph
+const workflow = new StateGraph(MessagesAnnotation)
+    .addNode('agent', agentModel)
+    .addEdge('__start__', 'agent')
+    .addEdge('agent','__end__');
+
+ const app = workflow.compile();
 
 async function main(params: string[]) {
     const rl = readline.createInterface({
@@ -12,7 +31,9 @@ async function main(params: string[]) {
         if(userInput.toLowerCase() === 'exit'){
             break;
         }
-        console.log(`Hello, ${userInput}!`);
+        const finalMessage = await app.invoke({messages:[userInput]});
+        const response = finalMessage.messages[finalMessage.messages.length - 1];
+        console.log('Bot:', response?.content);
     }
     rl.close();
     
