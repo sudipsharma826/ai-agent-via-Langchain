5adb384cd5432a2c60c0768a6f9b7ee25b609657
Sudip Sharma <sudpokhara826@gmail.com>
2025-09-09 12:21:35 +0545

    intergrate the user chat functionily with ai llm, tools Tavily and loading funcality,

--- index.ts
+++ index.ts
@@ -1,13 +1,30 @@
-import { AIMessage, HumanMessage } from '@langchain/core/messages';
+import { AIMessage, BaseMessage, HumanMessage } from '@langchain/core/messages';
 import { ChatGroq } from '@langchain/groq';
 import { StateGraph,MessagesAnnotation} from '@langchain/langgraph';
+import { ToolNode } from "@langchain/langgraph/prebuilt";
+import { TavilySearch } from "@langchain/tavily";
 import readline from 'node:readline/promises';
 import 'dotenv/config';
 
 interface MessageState {
-    messages: string[];
-
+    messages: BaseMessage[];
 }
+
+//Create the tool node
+const tool = new TavilySearch({
+  maxResults: 3,
+  topic: "general",
+  // includeAnswer: false,
+  // includeRawContent: false,
+  // includeImages: false,
+  // includeImageDescriptions: false,
+  // searchDepth: "basic",
+  // timeRange: "day",
+  // includeDomains: [],
+  // excludeDomains: [],
+});
+const tools= [tool];//can add multiple tools
+const toolNode = new ToolNode(tools);
 
 //Get the llm from the groq
 const groq = new ChatGroq({
@@ -15,23 +32,54 @@ const groq = new ChatGroq({
     temperature: 0,
     maxRetries: 3,
     apiKey: process.env.GROQ_API_KEY,
+}).bindTools(tools);//Binding the tools to the llm
 
-})
+
+//Function to decide whether to continue with llm or with tool
+function shouldContinue({ messages }: typeof MessagesAnnotation.State){
+    const lastMessage = messages[messages.length - 1] as  any;
+    // console.log('Last message:', lastMessage);
+    if (lastMessage instanceof AIMessage && lastMessage.tool_calls && lastMessage.tool_calls.length > 0) {
+        // console.log("Calling the tools");
+        return 'tools';
+    }
+    // console.log("Calling the llm");
+    return '__end__';
+}
 
 
 //Building a agent model and functionality
 async function agentModel(state: MessageState){
-    const response = await groq.invoke(state.messages);
-    return { messages: [ new AIMessage(response)] }; // returning updated state with new message
+    try{
+        const response = await groq.invoke(state.messages);
+        return { messages: [response] }; // returning updated state with new message
+    }catch(err){
+         return { 
+      messages: [new AIMessage("Sorry, I encountered an error processing your request.")] 
+    };
+    }
 }
 
 //Build the workflow uisng LangGraph
 const workflow = new StateGraph(MessagesAnnotation)
     .addNode('agent', agentModel)
     .addEdge('__start__', 'agent')
+    .addNode('tools', toolNode)//creating the tool node
+    .addEdge('tools','agent')
+    .addConditionalEdges('agent', shouldContinue)
     .addEdge('agent','__end__');
 
  const app = workflow.compile();
+
+ // Simple loading message functions
+function showLoading(mode: 'start' | 'stop') {
+  if (mode === 'start') {
+    process.stdout.write('Loading...');
+  } else {
+    process.stdout.clearLine(0);
+    process.stdout.cursorTo(0);
+  }
+}
 
 async function main(params: string[]) {
     const rl = readline.createInterface({
@@ -43,8 +91,10 @@ async function main(params: string[]) {
         if(userInput.toLowerCase() === 'exit'){
             break;
         }
+        showLoading('start');
         const finalMessage = await app.invoke({messages:[new HumanMessage(userInput)]});
         const response = finalMessage.messages[finalMessage.messages.length - 1];
+        showLoading('stop');
         console.log('Bot:', response?.content);
     }
     rl.close();
