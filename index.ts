74684ad60de40b52599b61e666ebe1daa3941a32
Sudip Sharma <sudpokhara826@gmail.com>
2025-09-09 07:33:54 +0545

    implemented the terminal input and output

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import readline from 'node:readline/promises';
+
+
+
+async function main(params: string[]) {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    while(true){
+        const userInput = await rl.question('You:');
+        if(userInput.toLowerCase() === 'exit'){
+            break;
+        }
+        console.log(`Hello, ${userInput}!`);
+    }
+    rl.close();
+    
+}
+main([]);
