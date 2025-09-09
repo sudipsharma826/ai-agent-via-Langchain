08f1c3fb4b72e90aa0fc4f9e99df239e61409cb4
Sudip Sharma <sudpokhara826@gmail.com>
2025-09-09 10:05:50 +0545

    added the groq and env in the application

--- index.ts
+++ index.ts
@@ -1,16 +1,28 @@
+import { AIMessage, HumanMessage } from '@langchain/core/messages';
+import { ChatGroq } from '@langchain/groq';
 import { StateGraph,MessagesAnnotation} from '@langchain/langgraph';
 import readline from 'node:readline/promises';
+import 'dotenv/config';
 
 interface MessageState {
     messages: string[];
 
 }
 
+//Get the llm from the groq
+const groq = new ChatGroq({
+    model: 'llama-3.1-8b-instant',
+    temperature: 0,
+    maxRetries: 3,
+    apiKey: process.env.GROQ_API_KEY,
+
+})
+
 
 //Building a agent model and functionality
-function agentModel(state: MessageState){
-    console.log("calling agent");
-    return state;
+async function agentModel(state: MessageState){
+    const response = await groq.invoke(state.messages);
+    return { messages: [ new AIMessage(response)] }; // returning updated state with new message
 }
 
 //Build the workflow uisng LangGraph
@@ -31,7 +43,7 @@ async function main(params: string[]) {
         if(userInput.toLowerCase() === 'exit'){
             break;
         }
-        const finalMessage = await app.invoke({messages:[userInput]});
+        const finalMessage = await app.invoke({messages:[new HumanMessage(userInput)]});
         const response = finalMessage.messages[finalMessage.messages.length - 1];
         console.log('Bot:', response?.content);
     }
